4be062385f8af8e2db2eb33a2e35b0c330b3f71b
satyam <satyambhargav17@gmail.com>
2023-09-29 16:43:31 +0530

    Categories-Tab-Favorite

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:meals/model/meal.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _showmessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _toggleMealFavoriteState(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+    if (isExisting) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showmessage('Meal is no longer a favorite');
+    } else {
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showmessage('Meal marked as favorite');
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(
+      onToggleFavorits: _toggleMealFavoriteState,
+    );
+    var activePageTitle = 'Categories';
+    if (_selectPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteState,
+      );
+      activePageTitle = 'Your Favorites';
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text(activePageTitle)),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          )
+        ],
+      ),
+    );
+  }
+}
